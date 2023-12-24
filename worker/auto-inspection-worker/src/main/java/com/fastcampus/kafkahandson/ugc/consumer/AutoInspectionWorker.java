6e908bd27a9edaa8e46df10a69c02e826b0fdcd4
HyunSangHan <gustkd3@snu.ac.kr>
2023-12-25 03:12:56 +0900

    검수를 거친 컨텐츠 kafka produce

--- worker/auto-inspection-worker/src/main/java/com/fastcampus/kafkahandson/ugc/consumer/AutoInspectionWorker.java
+++ worker/auto-inspection-worker/src/main/java/com/fastcampus/kafkahandson/ugc/consumer/AutoInspectionWorker.java
@@ -2,10 +2,12 @@ package com.fastcampus.kafkahandson.ugc.consumer;
 
 import com.fastcampus.kafkahandson.ugc.CustomObjectMapper;
 import com.fastcampus.kafkahandson.ugc.PostInspectUsecase;
+import com.fastcampus.kafkahandson.ugc.adapter.common.OperationType;
 import com.fastcampus.kafkahandson.ugc.adapter.common.Topic;
 import com.fastcampus.kafkahandson.ugc.adapter.originalpost.OriginalPostMessage;
 import com.fastcampus.kafkahandson.ugc.adapter.originalpost.OriginalPostMessageConverter;
 import com.fastcampus.kafkahandson.ugc.inspectedpost.model.InspectedPost;
+import com.fastcampus.kafkahandson.ugc.port.InspectedPostMessageProducePort;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -19,6 +21,7 @@ public class AutoInspectionWorker {
     private final CustomObjectMapper objectMapper = new CustomObjectMapper();
 
     private final PostInspectUsecase postInspectUsecase;
+    private final InspectedPostMessageProducePort inspectedPostMessageProducePort;
 
     @KafkaListener(
         topics = { Topic.ORIGINAL_POST },
@@ -27,9 +30,38 @@ public class AutoInspectionWorker {
     )
     public void listen(ConsumerRecord<String, String> message) throws JsonProcessingException {
         OriginalPostMessage originalPostMessage = objectMapper.readValue(message.value(), OriginalPostMessage.class);
+        if (originalPostMessage.getOperationType() == OperationType.CREATE) {
+            this.handleCreate(originalPostMessage);
+        } else if (originalPostMessage.getOperationType() == OperationType.UPDATE) {
+            this.handleUpdate(originalPostMessage);
+        } else if (originalPostMessage.getOperationType() == OperationType.DELETE) {
+            this.handleDelete(originalPostMessage);
+        }
+    }
+
+    private void handleCreate(OriginalPostMessage originalPostMessage) {
         InspectedPost inspectedPost = postInspectUsecase.inspectAndGetIfValid(
             OriginalPostMessageConverter.toModel(originalPostMessage)
         );
-        System.out.println(inspectedPost);
+        if (inspectedPost == null) {
+            return;
+        }
+        inspectedPostMessageProducePort.sendCreateMessage(inspectedPost);
+    }
+
+    private void handleUpdate(OriginalPostMessage originalPostMessage) {
+        InspectedPost inspectedPost = postInspectUsecase.inspectAndGetIfValid(
+            OriginalPostMessageConverter.toModel(originalPostMessage)
+        );
+        if (inspectedPost == null) {
+            inspectedPostMessageProducePort.sendDeleteMessage(originalPostMessage.getId());
+        } else {
+            inspectedPostMessageProducePort.sendUpdateMessage(inspectedPost);
+        }
+    }
+
+    private void handleDelete(OriginalPostMessage originalPostMessage) {
+        // DELETE 메시지는 검수가 필요 없으므로 바로 삭제
+        inspectedPostMessageProducePort.sendDeleteMessage(originalPostMessage.getId());
     }
 }
